a42e9b7a68c56225343a497c182469bdf21cc61e
Ankit0534 <137833229+Ankit0534@users.noreply.github.com>
2024-06-28 06:39:26 +0000

    container config done

---
+++ 01_basic/Datatype.js
@@ -0,0 +1,17 @@
+let Name = "Ankit"
+let age = 45
+ispass = false
+ 
+// string  name
+// number  age
+// boolean  true/false
+// null   object
+// undefine   undefine
+// symble   uniqe
+
+console.log(typeof ispass);//boolean
+console.log(typeof "Ankit");//string
+console.log(typeof age);//string
+console.log(typeof ispass);//string
+
+console.table(typeof[Name, age ,ispass, ])
