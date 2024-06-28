934dc44f55e907465f171fc2fd7e3ec70a3f1346
Ankit0534 <137833229+Ankit0534@users.noreply.github.com>
2024-06-28 06:05:33 +0000

    container config done

---
+++ 01_basic/variable.js
@@ -0,0 +1,6 @@
+const accountId = "0207464"
+let accountGmail = "ankit@google.com"
+var accountPassword = "Ankit0534"
+accountName = "ankit"
+
+console.table([accountId,accountGmail,accountPassword,accountName])
